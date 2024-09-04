6894f87c32c9f494ddafdf6f917e00e830999dd4
ksenia-t0809 <ksenia.t0809@gmail.com>
2024-09-04 22:56:03 +0300

    Components are completed

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    // Dropdown
+    $(document).mouseup(function (e) {
+        if (!$('.dropdown').is(e.target)
+        && $('.dropdown').has(e.target).length === 0)
+        {
+            $('.dropdown-button').removeClass('active');
+            $('.dropdown-menu').removeClass('show');
+       }
+    });
+    $('.dropdown').on('click', '.dropdown-button', function() {
+        $(this).toggleClass('active');
+        $(this).siblings('.dropdown-menu').toggleClass('show');
+    });
+    $('.dropdown-menu').on('click', '.dropdown-menu-item', function() {
+        $(this).parents('.dropdown-menu').find('.dropdown-menu-item').removeClass('selected');
+        $(this).addClass('selected');
+    });
+
+    // Search
+    $('.search').on('click', '.search-input', function() {
+        $(this).parents('.search').toggleClass('search-open')
+    });
+    $('.search-results .btn--category-full').on('click', function() {
+        $(this).toggleClass('btn--selected');
+    })
+    
+    
+})
